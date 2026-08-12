a6797e8d7e6aba6ebe8c1534363920e8a97a31ec
jboers892 <jelleboers@live.nl>
2026-08-12 22:22:28 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "workouts-v1";
+const CACHE = "workouts-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
